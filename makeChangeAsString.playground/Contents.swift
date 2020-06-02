bdd5f06631e53172d6b4caef75ad8c5898d8280d
Breena <breena_greek@live.com>
2020-06-01 23:15:28 -0400

    Initial Commit

--- makeChangeAsString.playground/Contents.swift
+++ makeChangeAsString.playground/Contents.swift
@@ -1,7 +1,67 @@
 import UIKit
 
+func changeString(change: Double) -> Int {
+    let changeString = Int(change)
+    return changeString
+}
 
+func makeChangeAsString(fromAmount: Double, withCost: Double) -> String {
+    var dollars: Int = 0
+    var quarters: Int = 0
+    var dimes: Int = 0
+    var nickels: Int = 0
+    var pennies: Int = 0
+    
+    let change = fromAmount - withCost
+    print(change)
+    
+    while change > 0 {
+        
+        if change > 1 {
+            let dollarChange = change.truncatingRemainder(dividingBy: 1)
+            print(dollarChange)
+            dollars = changeString(change: dollarChange)
+            
+            if dollarChange > 0.25 {
+                let quarterChange = dollarChange.truncatingRemainder(dividingBy: 4)
+                print(quarterChange)
+                quarters = changeString(change: quarterChange)
+                
+                if quarterChange > 0.10 {
+                    let dimeChange = quarterChange.truncatingRemainder(dividingBy: 10)
+                    print(dimeChange)
+                    dimes = changeString(change: dimeChange)
+                    
+                    if dimeChange > 0.05 {
+                        let nickelChange = dimeChange.truncatingRemainder(dividingBy: 25)
+                        print(nickelChange)
+                        nickels = changeString(change: nickelChange)
+                        
+                        if nickelChange > 0.01 {
+                            let pennyChange = dimeChange.truncatingRemainder(dividingBy: 100)
+                            print(pennyChange)
+                            pennies = changeString(change: pennyChange)
+                            
+                        }
+                    }
+                }
+            }
+        }
+    }
+        
+        
+        if change == 0 {
+            let printOut = "You have no change."
+            return printOut
+        } else {
+            let printOut = "Your change is $\(change). That is \(dollars) dollars, \(quarters) quarters, \(dimes) dimes, \(nickels) nickels, and \(pennies) pennies."
+            return printOut
+    }
 
+// I know this does work, though I did have it working better, so close though!
+    
+    makeChangeAsString(fromAmount: 5.00, withCost: 2.32)
+    makeChangeAsString(fromAmount: 5, withCost: 5)
 
 
 
